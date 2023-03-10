8d16b9ec8c20c2b7af5cf0aa016f968b792ac12d
MATHEUS HENRIQUE VITAL <vital.2907@aluno.pr.senac.br>
2023-03-10 08:57:05 -0300

    Lnde

---
+++ tabu.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+  $a=1;
+  $b=2;
+  $c=3;
+  $d=4;
+  $e=5;
+  $f=6;
+  $g=7;
+  $h=8;
+  $i=9;
+  $j=10;
+  do {
+    echo "Numero: " . $a . "<br/>";
+    $a = $a + 1;
+  }
+  while ($a <= 10);
+  echo "<br/>";
+
+  do {
+    echo "Numero: " . $b . "</br>";
+    $b = $b + 2;
+  }
+  while ($b <= 20); 
+  echo "<br/>";
+  do {
+    echo "Numero: " . $c . "</br>";
+    $c = $c + 3;
+  }
+  while ($c <= 30);
+  echo "<br/>";
+  do {
+    echo "Numero: " . $d . "</br>";
+    $d = $d + 4;
+  }
+  while ($d <= 40);
+  echo "<br/>";
+  do {
+    echo "Numero: " . $e . "</br>";
+    $e = $e + 5;
+  }
+  while ($e <= 50);
+  
+  do {
+    echo "Numero: " . $f . "</br>";
+    $f = $f + 6;
+  }
+  while ($f <= 60);
+  echo "<br/>";
+  do {
+    echo "Numero: " . $g . "</br>";
+    $g = $g + 7;
+  }
+  while ($g <= 70);
+  echo "<br/>";
+  do {
+    echo "Numero: " . $h . "</br>";
+    $h = $h + 8;
+  }
+  while ($h <= 80);
+  echo "<br/>";
+  do {
+    echo "Numero: " . $i . "</br>";
+    $i = $i + 9;
+  }
+  while ($i <= 90);
+  echo "<br/>";
+  do {
+    echo "Numero: " . $j . "</br>";
+    $j = $j + 10;
+  }
+  while ($j <= 100);
+?>
+</body>
+</html>
